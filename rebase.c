b493046134d2e593df689ede62d037eaac8ac8e0
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-05-14 12:11:24 +0200

    pull: remove support for `--rebase=preserve`
    In preparation for `git-rebase--preserve-merges.sh` entering its after
    life, we remove this (deprecated) option that would still rely on it.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- rebase.c
+++ rebase.c
@@ -20,8 +20,6 @@ enum rebase_type rebase_parse_value(const char *value)
 		return REBASE_FALSE;
 	else if (v > 0)
 		return REBASE_TRUE;
-	else if (!strcmp(value, "preserve") || !strcmp(value, "p"))
-		return REBASE_PRESERVE;
 	else if (!strcmp(value, "merges") || !strcmp(value, "m"))
 		return REBASE_MERGES;
 	else if (!strcmp(value, "interactive") || !strcmp(value, "i"))
