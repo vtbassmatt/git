42633b8d03008a159bc42bde319f50e87ddb00f6
Elijah Newren <newren@gmail.com>
2020-08-20 14:52:27 -0700

    strmap: add more utility functions
    This adds a number of additional convienence functions I want/need:
      * strmap_get_size()
      * strmap_empty()
      * strmap_remove()
      * strmap_for_each_entry()
      * strmap_get_entry()
    
    I suspect the first four are self-explanatory.
    
    strmap_get_entry() is similar to strmap_get() except that instead of just
    returning the void* value that the string maps to, it returns the
    strmap_entry that contains both the string and the void* value (or
    NULL if the string isn't in the map).  This is helpful because it avoids
    multiple lookups, e.g. in some cases a caller would need to call:
      * strmap_contains() to check that the map has an entry for the string
      * strmap_get() to get the void* value
      * <do some work to update the value>
      * strmap_put() to update/overwrite the value
    If the void* pointer returned really is a pointer, then the last step is
    unnecessary, but if the void* pointer is just cast to an integer then
    strmap_put() will be needed.  In contrast, one can call strmap_get_entry()
    and then:
      * check if the string was in the map by whether the pointer is NULL
      * access the value via entry->value
      * directly update entry->value
    meaning that we can replace two or three hash table lookups with one.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- strmap.h
+++ strmap.h
@@ -51,6 +51,12 @@ void strmap_clear(struct strmap *map, int free_values);
 void *strmap_put(struct strmap *map, const char *str, void *data);
 
 /*
+ * Return the strmap_entry mapped by "str", or NULL if there is not such
+ * an item in map.
+ */
+struct strmap_entry *strmap_get_entry(struct strmap *map, const char *str);
+
+/*
  * Return the data pointer mapped by "str", or NULL if the entry does not
  * exist.
  */
@@ -61,5 +67,35 @@ void *strmap_get(struct strmap *map, const char *str);
  * strmap_get() in that it can distinguish entries with a NULL data pointer.
  */
 int strmap_contains(struct strmap *map, const char *str);
+
+/*
+ * Remove the given entry from the strmap.  If the string isn't in the
+ * strmap, the map is not altered.
+ */
+void strmap_remove(struct strmap *map, const char *str, int free_value);
+
+/*
+ * Return how many entries the strmap has.
+ */
+static inline unsigned int strmap_get_size(struct strmap *map)
+{
+	return hashmap_get_size(&map->map);
+}
+
+/*
+ * Return whether the strmap is empty.
+ */
+static inline int strmap_empty(struct strmap *map)
+{
+	return strmap_get_size(map) == 0;
+}
+
+/*
+ * iterate through @map using @iter, @var is a pointer to a type strmap_entry
+ */
+#define strmap_for_each_entry(mystrmap, iter, var)	\
+	for (var = hashmap_iter_first_entry_offset(&(mystrmap)->map, iter, 0); \
+		var; \
+		var = hashmap_iter_next_entry_offset(iter, 0))
 
 #endif /* STRMAP_H */
