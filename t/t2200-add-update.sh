0a1550cb225591a53118bd8f7782d95de5a4df34
Amanda  Shafack <shafack.likhene@gmail.com>
2020-10-17 15:51:24 +0100

    t2200: modify code syntax
    Using the logical not operator on both the git and grep command is
    redundant, since the main check is done at the level of the grep
    command.
    
    Apply the logical not operator only to the grep command.
    
    Co-authored-by: Johannes Schindelin <dscho@github.com>
    Signed-off-by: Amanda Shafack <shafack.likhene@gmail.com>

--- t/t2200-add-update.sh
+++ t/t2200-add-update.sh
@@ -179,10 +179,8 @@ test_expect_success 'add -u resolves unmerged paths' '
 
 test_expect_success '"add -u non-existent" should fail' '
 	test_must_fail git add -u non-existent &&
-	! (
-		git ls-files >actual &&
-		grep "non-existent" actual
-	)
+	git ls-files >actual &&
+	! grep "non-existent" actual
 '
 
 test_done
