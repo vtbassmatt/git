2aa44c1f13ac1fd70167770f7749611eadc396d4
Elijah Newren <newren@gmail.com>
2020-10-20 19:44:45 -0700

    merge tests: expect slight differences in output for recursive vs. ort
    The ort merge strategy has some slight differences in commit
    descriptions (shortened hashes), stdout vs stderr, and in conflict
    messages.  Also, builtin/merge.c reports usage of "ort" as "Merge made
    by the 'ort' strategy" -- while it is meant as a drop in replacement for
    "recursive" it is not yet treated as though it is recursive.  Update the
    testcases to expect different output for the different merge backends.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- t/t7602-merge-octopus-many.sh
+++ t/t7602-merge-octopus-many.sh
@@ -77,6 +77,10 @@ Merge made by the 'recursive' strategy.
 EOF
 
 test_expect_success 'merge reduces irrelevant remote heads' '
+	if test "$GIT_TEST_MERGE_ALGORITHM" = ort
+	then
+		sed -i'' s/recursive/ort/ expected
+	fi &&
 	GIT_MERGE_VERBOSITY=0 git merge c4 c5 >actual &&
 	test_i18ncmp expected actual
 '
