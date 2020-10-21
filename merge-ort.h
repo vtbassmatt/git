b9e73975eab1f349be678779ff57155feb4c3501
Elijah Newren <newren@gmail.com>
2020-10-20 21:59:26 -0700

    merge-ort: barebones API of new merge strategy with empty implementation
    This is the beginning of a new merge strategy.  While there are some API
    differences, and the implementation has some differences in behavior, it
    is essentially meant as an eventual drop-in replacement for
    merge-recursive.c.  However, it is being built to exist side-by-side
    with merge-recursive so that we have plenty of time to find out how
    those differences pan out in the real world while people can still fall
    back to merge-recursive.  (Also, I intend to avoid modifying
    merge-recursive during this process, to keep it stable.)
    
    The primary difference noticable here is that the updating of the
    working tree and index is not done simultaneously with the merge
    algorithm, but is a separate post-processing step.  The new API is
    designed so that one can do repeated merges (e.g. during a rebase or
    cherry-pick) and only update the index and working tree one time at the
    end instead of updating it with every intermediate result.  Also, one
    can perform a merge between two branches, neither of which match the
    index or the working tree, without clobbering the index or working tree.
    
    The next three commits will demonstrate various uses of this new API.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

---
+++ merge-ort.h
@@ -0,0 +1,49 @@
+#ifndef MERGE_ORT_H
+#define MERGE_ORT_H
+
+#include "merge-recursive.h"
+
+struct commit;
+struct tree;
+
+struct merge_result {
+	/* whether the merge is clean */
+	int clean;
+
+	/* Result of merge.  If !clean, represents what would go in worktree */
+	struct tree *tree;
+
+	/*
+	 * Additional metadata used by merge_switch_to_result() or future calls
+	 * to merge_inmemory_*().  Not for external use.
+	 */
+	void *priv;
+	unsigned ate;
+};
+
+/* rename-detecting three-way merge with recursive ancestor consolidation. */
+void merge_inmemory_recursive(struct merge_options *opt,
+			      struct commit_list *merge_bases,
+			      struct commit *side1,
+			      struct commit *side2,
+			      struct merge_result *result);
+
+/* rename-detecting three-way merge, no recursion. */
+void merge_inmemory_nonrecursive(struct merge_options *opt,
+				 struct tree *merge_base,
+				 struct tree *side1,
+				 struct tree *side2,
+				 struct merge_result *result);
+
+/* Update the working tree and index from head to result after inmemory merge */
+void merge_switch_to_result(struct merge_options *opt,
+			    struct tree *head,
+			    struct merge_result *result,
+			    int update_worktree_and_index,
+			    int display_update_msgs);
+
+/* Do needed cleanup when not calling merge_switch_to_result() */
+void merge_finalize(struct merge_options *opt,
+		    struct merge_result *result);
+
+#endif
