5a3acf811998bb728ce94c69611c237861775142
Derrick Stolee <dstolee@microsoft.com>
2020-11-19 09:14:53 -0500

    maintenance: use 'git config --fixed-value'
    When a repository's leading directories contain regex glob characters,
    the config calls for 'git maintenance register' and 'git maintenance
    unregister' are not careful enough. Use the new --fixed-value option
    to direct the config machinery to use exact string matches. This is a
    more robust option than excaping these arguments in a piecemeal fashion.
    
    For the test, require that we are not running on Windows since the '+'
    character is not allowed on that filesystem.
    
    Reported-by: Emily Shaffer <emilyshaffer@google.com>
    Reported-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- t/t7900-maintenance.sh
+++ t/t7900-maintenance.sh
@@ -367,6 +367,18 @@ test_expect_success 'register and unregister' '
 	test_cmp before actual
 '
 
+test_expect_success !MINGW 'register and unregister with glob characters' '
+	GLOB="a+b*c" &&
+	git init "$GLOB" &&
+	git -C "$GLOB" maintenance register &&
+	git config --get-all --show-origin maintenance.repo &&
+	git config --get-all --global --fixed-value \
+		maintenance.repo "$(pwd)/$GLOB" &&
+	git -C "$GLOB" maintenance unregister &&
+	test_must_fail git config --get-all --global --fixed-value \
+		maintenance.repo "$(pwd)/$GLOB"
+'
+
 test_expect_success 'start from empty cron table' '
 	GIT_TEST_CRONTAB="test-tool crontab cron.txt" git maintenance start &&
 
