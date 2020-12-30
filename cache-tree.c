5d1c9c8a356b9003be21059c7ed6232732fd26c0
Derrick Stolee <dstolee@microsoft.com>
2020-12-30 11:35:09 -0500

    cache-tree: trace regions for prime_cache_tree
    Commands such as "git reset --hard" rebuild the in-memory representation
    of the cached tree index extension by parsing tree objects starting at a
    known root tree. The performance of this operation can vary widely
    depending on the width and depth of the repository's working directory
    structure. Measure the time in this operation using trace2 regions in
    prime_cache_tree().
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- cache-tree.c
+++ cache-tree.c
@@ -746,7 +746,10 @@ void prime_cache_tree(struct repository *r,
 {
 	cache_tree_free(&istate->cache_tree);
 	istate->cache_tree = cache_tree();
+
+	trace2_region_enter("cache-tree", "prime_cache_tree", the_repository);
 	prime_cache_tree_rec(r, istate->cache_tree, tree);
+	trace2_region_leave("cache-tree", "prime_cache_tree", the_repository);
 	istate->cache_changed |= CACHE_TREE_CHANGED;
 }
 
