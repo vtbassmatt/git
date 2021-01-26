e3475633e1d2e397c5095f34c23a19fccb2c8428
Derrick Stolee <dstolee@microsoft.com>
2021-01-26 09:18:08 -0500

    chunk-format: create read chunk API
    Add the capability to read the table of contents, then pair the chunks
    with necessary logic using read_chunk_fn pointers. Callers will be added
    in future changes, but the typical outline will be:
    
     1. initialize a 'struct chunkfile' with init_chunkfile(NULL).
     2. call read_table_of_contents().
     3. for each chunk to parse, call pair_chunk() with appropriate pointers.
     4. call free_chunkfile() to clear the 'struct chunkfile' data.
    
    We are re-using the anonymous 'struct chunkfile' data, as it is internal
    to the chunk-format API. This gives it essentially two modes: write and
    read. If the same struct instance was used for both reads and writes,
    then there would be failures.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- chunk-format.h
+++ chunk-format.h
@@ -17,4 +17,25 @@ void add_chunk(struct chunkfile *cf,
 	       size_t size);
 int write_chunkfile(struct chunkfile *cf, void *data);
 
+int read_table_of_contents(struct chunkfile *cf,
+			   const unsigned char *mfile,
+			   size_t mfile_size,
+			   uint64_t toc_offset,
+			   int toc_length);
+
+/*
+ * When reading a table of contents, we find the chunk with matching 'id'
+ * then call its read_fn to populate the necessary 'data' based on the
+ * chunk start and size.
+ */
+typedef int (*chunk_read_fn)(const unsigned char *chunk_start,
+			     size_t chunk_size, void *data);
+
+
+#define CHUNK_NOT_FOUND (-2)
+int pair_chunk(struct chunkfile *cf,
+	       uint32_t chunk_id,
+	       chunk_read_fn fn,
+	       void *data);
+
 #endif
