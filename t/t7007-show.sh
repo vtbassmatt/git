2657fb9e92b462a0684c57d47ea96885585c52a1
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-16 00:43:43 +0100

    show tests: add test for "git show <tree>"
    Add missing tests for showing a tree with "git show". Let's test for
    showing a tree, two trees, and that doing so doesn't recurse.
    
    The only tests for this code added in 5d7eeee2ac6 (git-show: grok
    blobs, trees and tags, too, 2006-12-14) were the tests in
    t7701-repack-unpack-unreachable.sh added in ccc1297226b (repack:
    modify behavior of -A option to leave unreferenced objects unpacked,
    2008-05-09).
    
    Let's add this common mode of operation to the "show" tests
    themselves. It's more obvious, and the tests in
    t7701-repack-unpack-unreachable.sh happily parse if we start buggily
    emitting trees recursively.
    
    Signed-off-by: Ã†var ArnfjÃ¶rÃ° Bjarmason <avarab@gmail.com>
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- t/t7007-show.sh
+++ t/t7007-show.sh
@@ -38,6 +38,45 @@ test_expect_success 'showing two commits' '
 	test_cmp expect actual.filtered
 '
 
+test_expect_success 'showing a tree' '
+	cat >expected <<-EOF &&
+	tree main1:
+
+	main1.t
+	EOF
+	git show main1: >actual &&
+	test_cmp expected actual
+'
+
+test_expect_success 'showing two trees' '
+	cat >expected <<-EOF &&
+	tree main1:
+
+	main1.t
+
+	tree main2:
+
+	main1.t
+	main2.t
+	EOF
+	git show main1: main2: >actual &&
+	test_cmp expected actual
+'
+
+test_expect_success 'showing a trees is not recursive' '
+	git worktree add not-recursive main1 &&
+	mkdir not-recursive/a &&
+	test_commit -C not-recursive a/file &&
+	cat >expected <<-EOF &&
+	tree a/file:
+
+	a/
+	main1.t
+	EOF
+	git -C not-recursive show a/file: >actual &&
+	test_cmp expected actual
+'
+
 test_expect_success 'showing a range walks (linear)' '
 	cat >expect <<-EOF &&
 	commit $(git rev-parse main3)
