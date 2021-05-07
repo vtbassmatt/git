44a1322c44026e675ea254a00f3b50d4955ac56e
Elijah Newren <newren@gmail.com>
2021-05-06 17:25:18 -0700

    [RFC] ls-files: error out on -i unless -o or -c are specified
    ls-files --ignored can be used together with either --others or
    --cached.  After being perplexed for a bit and digging in to the code, I
    assumed that ls-files -i was just broken and not printing anything and
    had a nice patch ready to submit when I finally realized that -i can be
    used with --cached to find tracked ignores.
    
    While that was a mistake on my part, and a careful reading of the
    documentation could have made this more clear, I suspect this is an
    error others are likely to make as well.  In fact, of two uses in our
    testsuite, I believe one of the two did make this error.  In t1306.13,
    there are NO tracked files, and all the excludes built up and used in
    that test and in previous tests thus have to be about untracked files.
    However, since they were looking for an empty result, the mistake went
    unnoticed as their erroneous command also just happened to give an empty
    answer.
    
    -i will most the time be used with -o, which would suggest we could just
    make -i imply -o in the absence of either a -o or -c, but that would be
    a backward incompatible break.  Instead, let's just flag -i without
    either a -o or -c as an error, and update the two relevant testcases to
    specify their intent.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- builtin/ls-files.c
+++ builtin/ls-files.c
@@ -748,6 +748,9 @@ int cmd_ls_files(int argc, const char **argv, const char *cmd_prefix)
 	if (pathspec.nr && error_unmatch)
 		ps_matched = xcalloc(pathspec.nr, 1);
 
+	if ((dir.flags & DIR_SHOW_IGNORED) && !show_others && !show_cached)
+		die("ls-files --ignored is usually used with --others, but --cached is the default.  Please specify which you want.");
+
 	if ((dir.flags & DIR_SHOW_IGNORED) && !exc_given)
 		die("ls-files --ignored needs some exclude pattern");
 
