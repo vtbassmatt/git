d48207d6858502f04fd501a24ff7c2a80062dfbe
Han-Wen Nienhuys <hanwen@google.com>
2021-08-16 20:17:17 +0200

    test-ref-store: tweaks to for-each-reflog-ent format
    Follow the reflog format more closely, so it can be used for comparing
    reflogs in tests without using inspecting files under .git/logs/
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>

--- t/helper/test-ref-store.c
+++ t/helper/test-ref-store.c
@@ -151,9 +151,9 @@ static int each_reflog(struct object_id *old_oid, struct object_id *new_oid,
 		       const char *committer, timestamp_t timestamp,
 		       int tz, const char *msg, void *cb_data)
 {
-	printf("%s %s %s %"PRItime" %d %s\n",
-	       oid_to_hex(old_oid), oid_to_hex(new_oid),
-	       committer, timestamp, tz, msg);
+	const char *newline = strchr(msg, '\n') ? "" : "\n";
+	printf("%s %s %s %" PRItime " %+05d\t%s%s", oid_to_hex(old_oid),
+	       oid_to_hex(new_oid), committer, timestamp, tz, msg, newline);
 	return 0;
 }
 
