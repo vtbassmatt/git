d8843185fe46e0c4e869159f9539714c5d232810
Neeraj Singh <neerajsi@microsoft.com>
2021-08-26 12:35:27 -0700

    update-index: use the bulk-checkin infrastructure
    The update-index functionality is used internally by 'git stash push' to
    setup the internal stashed commit.
    
    This change enables bulk-checkin for update-index infrastructure to
    speed up adding new objects to the object database by leveraging the
    pack functionality and the new bulk-fsync functionality. This mode
    is enabled when passing paths to update-index via the --stdin flag,
    as is done by 'git stash'.
    
    There is some risk with this change, since under batch fsync, the object
    files will not be available until the update-index is entirely complete.
    This usage is unlikely, since any tool invoking update-index and
    expecting to see objects would have to snoop the output of --verbose to
    find out when update-index has actually processed a given path.
    Additionally the index is locked for the duration of the update.
    
    Signed-off-by: Neeraj Singh <neerajsi@microsoft.com>

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -5,6 +5,7 @@
  */
 #define USE_THE_INDEX_COMPATIBILITY_MACROS
 #include "cache.h"
+#include "bulk-checkin.h"
 #include "config.h"
 #include "lockfile.h"
 #include "quote.h"
@@ -1152,6 +1153,7 @@ int cmd_update_index(int argc, const char **argv, const char *prefix)
 		struct strbuf unquoted = STRBUF_INIT;
 
 		setup_work_tree();
+		plug_bulk_checkin();
 		while (getline_fn(&buf, stdin) != EOF) {
 			char *p;
 			if (!nul_term_line && buf.buf[0] == '"') {
@@ -1166,6 +1168,7 @@ int cmd_update_index(int argc, const char **argv, const char *prefix)
 				chmod_path(set_executable_bit, p);
 			free(p);
 		}
+		unplug_bulk_checkin(&lock_file);
 		strbuf_release(&unquoted);
 		strbuf_release(&buf);
 	}
