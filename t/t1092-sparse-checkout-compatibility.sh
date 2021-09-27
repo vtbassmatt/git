e7cda32efb63c63cf60513ec447418fd97ab3fa9
Victoria Dye <vdye@github.com>
2021-09-27 15:55:43 -0400

    reset: make --mixed sparse-aware
    Sparse directory entries are "diffed" as trees in `diff_cache` (used
    internally by `reset --mixed`), following a code path separate from
    individual file handling. The use of `diff_tree_oid` there requires setting
    explicit `change` and `add_remove` functions to process the internal
    contents of a sparse directory.
    
    Additionally, the `recursive` diff option handles cases in which `reset
    --mixed` must diff/merge files that are nested multiple levels deep in a
    sparse directory.
    
    Signed-off-by: Victoria Dye <vdye@github.com>

--- t/t1092-sparse-checkout-compatibility.sh
+++ t/t1092-sparse-checkout-compatibility.sh
@@ -801,14 +801,25 @@ test_expect_success 'sparse-index is not expanded' '
 	for ref in update-deep update-folder1 update-folder2 update-deep
 	do
 		echo >>sparse-index/README.md &&
+		ensure_not_expanded reset --mixed $ref
 		ensure_not_expanded reset --hard $ref || return 1
 	done &&
 
 	ensure_not_expanded reset --hard update-deep &&
 	ensure_not_expanded reset --keep base &&
 	ensure_not_expanded reset --merge update-deep &&
-	ensure_not_expanded reset --hard &&
 
+	ensure_not_expanded reset base -- deep/a &&
+	ensure_not_expanded reset base -- nonexistent-file &&
+	ensure_not_expanded reset deepest -- deep &&
+
+	# Although folder1 is outside the sparse definition, it exists as a
+	# directory entry in the index, so it will be reset without needing to
+	# expand the full index.
+	ensure_not_expanded reset --hard update-folder1 &&
+	ensure_not_expanded reset base -- folder1 &&
+
+	ensure_not_expanded reset --hard update-deep &&
 	ensure_not_expanded checkout -f update-deep &&
 	test_config -C sparse-index pull.twohead ort &&
 	(
