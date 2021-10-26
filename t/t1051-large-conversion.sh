d87d4229bb4a5ed6903d277d38899b4f8f592c00
Matt Cooper <vtbassmatt@gmail.com>
2021-10-26 06:45:41 -0400

    clean/smudge: allow clean filters to process extremely large files
    The filter system allows for alterations to file contents when they're
    moved between the database and the worktree. We already made sure that
    it is possible for smudge filters to produce contents that are larger
    than `unsigned long` can represent (which matters on systems where
    `unsigned long` is narrower than `size_t`, most notably 64-bit Windows).
    Now we make sure that clean filters can _consume_ contents that are
    larger than that.
    
    Note that this commit only allows clean filters' _input_ to be larger
    than can be represented by `unsigned long`.
    
    This change makes only a very minute dent into the much larger project
    to teach Git to use `size_t` instead of `unsigned long` wherever
    appropriate.
    
    Helped-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Matt Cooper <vtbassmatt@gmail.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t1051-large-conversion.sh
+++ t/t1051-large-conversion.sh
@@ -96,4 +96,14 @@ test_expect_success EXPENSIVE,!LONG_IS_64BIT 'files over 4GB convert on output' 
 	test "$size" -ge $((5 * 1024 * 1024 * 1024))
 '
 
+# This clean filter writes down the size of input it receives. By checking against
+# the actual size, we ensure that cleaning doesn't mangle large files on 64-bit Windows.
+test_expect_success EXPENSIVE,!LONG_IS_64BIT 'files over 4GB convert on input' '
+	test-tool genzeros $((5*1024*1024*1024)) >big &&
+	test_config filter.checklarge.clean "wc -c >big.size" &&
+	echo "big filter=checklarge" >.gitattributes &&
+	git add big &&
+	test $(test_file_size big) -eq $(cat big.size)
+'
+
 test_done
