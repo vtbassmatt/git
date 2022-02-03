493254ffbb8b11f325f5995790341d70198b5c97
Phillip Wood <phillip.wood@dunelm.org.uk>
2022-02-03 11:01:47 +0000

    rebase --merge: fix reflog when continuing
    The reflog message for a conflict resolution committed by "rebase
    --continue" looks like
    
    	rebase (continue): commit subject line
    
    Unfortunately the reflog message each subsequent pick look like
    
    	rebase (continue) (pick): commit subject line
    
    Fix this by setting the reflog message for "rebase --continue" in
    sequencer_continue() so it does not affect subsequent commits. This
    introduces a memory leak similar to the one leaking GIT_REFLOG_ACTION
    in pick_commits(). Both of these will be fixed in a future series that
    stops the sequencer calling setenv().
    
    Signed-off-by: Phillip Wood <phillip.wood@dunelm.org.uk>

--- builtin/rebase.c
+++ builtin/rebase.c
@@ -1247,8 +1247,6 @@ int cmd_rebase(int argc, const char **argv, const char *prefix)
 		int fd;
 
 		options.action = "continue";
-		set_reflog_action(&options);
-
 		/* Sanity check */
 		if (get_oid("HEAD", &head))
 			die(_("Cannot read HEAD"));
